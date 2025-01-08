c1f63faa4318bc465ad59e59643eae385ba79bfd
jk1mn <alexander.erm1006@gmail.com>
2025-01-08 11:48:17 +0700

    Init

--- -css/counter.css.ts
+++
@@ -1,3 +0,0 @@
-namespace $ { $mol_style_attach( "my/counter/counter.css",
- "/* mam/my/counter/counter.css */\n[my_lom_theme=\"light\"] {\n\tbackground-color: white;\n}\n\n[my_lom_theme=\"dark\"] {\n\tbackground-color: black;\n}\n"
-) }
